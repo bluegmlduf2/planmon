9fbf17f58b0f194e08ed45d7bca0248058e2d84b
wally_yoon <bluegmlduf2@naver.com>
2022-05-01 17:47:12 +0900

    0501 토스트 속성 변경 닫기버튼보이게

--- src/plugins/vue-toastification.js
+++ src/plugins/vue-toastification.js
@@ -17,13 +17,13 @@ const options = {
   maxToasts: 10, // 최대 메세지표시수
   newestOnTop: true, // 새로운메세지를 상단에 표시
   position: 'top-center', // 위치
-  timeout: 3000, // 표시시간
+  timeout: 2700, // 표시시간
   closeOnClick: true, // 닫기버튼사용유무
   pauseOnFocusLoss: true, // 화면에 포커스를 놓치면 프로그레스가 멈춘다
   pauseOnHover: true, // 마우스를 올리면 프로그레스가 멈춘다
   draggable: true, // 드래그가능
   draggablePercent: 0.6, // 드래그종료퍼센트
-  showCloseButtonOnHover: true, // 마우스 올리면 종료버튼을 보여준다
+  // showCloseButtonOnHover: true, // 마우스 올리면 종료버튼을 보여준다
   hideProgressBar: false, // 프로그레스바사용유무
   closeButton: 'button',
   icon: false, // 아이콘사용유무
