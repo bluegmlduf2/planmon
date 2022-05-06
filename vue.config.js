e2c6925f4f9c8961ad4ed543cd5c8039cc02ff98
wally_yoon <bluegmlduf2@naver.com>
2022-05-06 18:34:47 +0900

    0506 vue와 flask연결작업 기본설정1

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     proxy: {
       '/api*': {
         // changeOrigin는 CORS위반을 막기 위해 서버의 URI(http://localhost:5000)로 출처를 바꿈
-        target: 'http://localhost:5000/api',
+        target: 'http://127.0.0.1:5000/',
         changeOrigin: true,
       },
     },
