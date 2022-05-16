b32b135860cee4963e52a5159cd2f9ba3b81d6ab
wally_yoon <bluegmlduf2@naver.com>
2022-05-17 00:48:58 +0900

    0517 할일일정화면 초기화

--- src/views/Todolist/Index.vue
+++ src/views/Todolist/Index.vue
@@ -6,7 +6,7 @@
     >
       <!-- 다가오는 일정 -->
       <v-list
-        :param-list="todoList"
+        :param-list="todolist"
         :param-show-buttons="true"
         :param-is-add="true"
         @updateCheckInput="selectTodoCheckInput"
@@ -45,7 +45,7 @@
 import VLayout from '@/layouts/Default.vue';
 import VList from '@/components/List.vue';
 import VSearch from '@/components/Search.vue';
-import ListProxy from '@/proxies/ListProxy';
+import TodoListProxy from '@/proxies/TodoListProxy';
 
 export default {
   /**
@@ -64,24 +64,46 @@ export default {
 
   data() {
     return {
-      todoList: [], // 다가오는 일정 리스트
+      todolist: [], // 다가오는 일정 리스트
     };
+  },
+  computed: {
+    // 유저정보 (store에서 값이 변경될때마다 갱신)
+    user() {
+      return this.$store.getters.user;
+    },
+    // 유저 선택 정보
+    selection() {
+      return this.$store.getters.selection;
+    },
   },
   created() {
     this.initTodoList(); // 다가오는 일정 초기화 (최대 5개 호출)
   },
-
   methods: {
     // 다가오는 일정 초기화
     initTodoList() {
-      new ListProxy()
-        .getTodoList()
-        .then((response) => {
-          this.todoList = response.data;
-        })
-        .catch(() => {
-          console.log('Request failed...');
-        });
+      if (this.user) {
+        // 로그인상태일시 나의 할일 일정 취득
+        new TodoListProxy()
+          .getMyTodoList()
+          .then((response) => {
+            this.todolist = response.data;
+          })
+          .catch(() => {
+            console.log('Request failed...');
+          });
+      } else {
+        // 미로그인시
+        new TodoListProxy()
+          .getTodoList(this.selection)
+          .then((response) => {
+            this.todolist = response.data;
+          })
+          .catch(() => {
+            console.log('Request failed...');
+          });
+      }
     },
     // 다가오는 일정 체크박스 선택
     selectTodoCheckInput(param) {
