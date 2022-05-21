eea11ed4298ca1bb1cfcf33f50f70f11a3f2f653
wally_yoon <bluegmlduf2@naver.com>
2022-05-22 01:23:25 +0900

    0522 추천일정 오류 수정

--- app/main/service/rec_list_service.py
+++ app/main/service/rec_list_service.py
@@ -5,8 +5,11 @@ from pytz import timezone
 
 def get_reclist(selection):
     '''유저의 할일 리스트 취득'''
-    filters = {k: v for k, v in selection.items() if v is not None} # 빈 검색조건 제외
-    del filters['entryDate'] # 검색조건의 입국일자 제외
+    # 빈 검색조건 제외
+    filters = {k: v for k, v in selection.items() if v is not None} 
+    # 검색조건의 입국일자 존재시 제외
+    if filters.get('entryDate'):
+         del filters['entryDate']
     my_reclist = List.query.filter_by(**filters).order_by(List.createdDate.desc()).all() # 추천 일정 취득
 
     # 체류중인상태에서 입국후 지난 날짜의 조건이 가까운 순서로 조회
@@ -16,7 +19,7 @@ def get_reclist(selection):
         currentDate = datetime.strptime(datetime.now(timezone('Asia/Seoul')).strftime(dateFormat),dateFormat) # 현재시간
         # 만약 입국날짜가 현재시간보다 미래일 경우 일반 추천 일정반환
         if currentDate < entryDate:
-            return
+            return my_reclist
         diff = (currentDate-entryDate).days # 입국후 경과 일수
         # 사용자에게 유효한 추천일정을 상위에 표시한다
         # 아래의 로직은 일정의 인덱스 위치를 변경하는 로직
@@ -27,9 +30,10 @@ def get_reclist(selection):
             if diff > x.afterEntryDate:
                 added_item.append(x)
                 deleted_item.append(x)
-        # 삭제한 아이템 삭제하기
+        # 삭제한 아이템을 추천일정으로부터 삭제하기
         for x in deleted_item:
             my_reclist.remove(x)
+        # 변경한 순서의 리스트를 재할당 
         my_reclist = my_reclist+added_item
 
     return my_reclist
