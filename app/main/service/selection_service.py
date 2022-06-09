e68737a7f1c8601235c092a6dee4b4b8b5ab3e41
wally_yoon <bluegmlduf2@naver.com>
2022-06-09 23:45:00 +0900

    0609 selection의 초기값 취득 버그 수정

--- app/main/service/selection_service.py
+++ app/main/service/selection_service.py
@@ -5,8 +5,8 @@ from app.main.service.complete_list_service import get_my_completelist
 def get_a_selection(uid):
     '''유저선택정보를 취득'''
     user=User.query.filter_by(uid=uid).first()
-    todolist=get_my_todolist(uid)  # 내 리스트정보에서 할일 리스트 취득
-    completelist=get_my_completelist(uid)  # 내 리스트정보에서 할일 리스트 취득
+    todolist=get_my_todolist(uid)['my_todolist']  # 내 리스트정보에서 할일 리스트 취득
+    completelist=get_my_completelist(uid)['my_completelist']  # 내 리스트정보에서 할일 리스트 취득
     setattr(user,'myTodolist',todolist) # 유저 객체에 할일 리스트 등록
     setattr(user,'myCompletelist',completelist) # 유저 객체에 완료 리스트 등록
     return user
