91671431ea2ffbad1676631e6f47da1726f24aca
wally_yoon <bluegmlduf2@naver.com>
2022-06-19 20:28:07 +0900

    0619 순환참조에러 방지를 위해 수정

--- app/main/util/__init__.py
+++ app/main/util/__init__.py
@@ -1,4 +1,3 @@
-from app.main.model.list import List
 from datetime import datetime
 from sqlalchemy import case
 from pytz import timezone
@@ -14,6 +13,9 @@ def get_current_time():
 
 def sort_by_id(postIds):
     '''매개변수로 전달된 리스트의 순서를 postIds의 순서에 맞춰 변경한다'''
+    # 순환참조 List를 import시 발생하는 순환참조에러 방지를 위해 아래와같이 import (circular import)
+    from app.main.model.list import List
+    
     # 사용자 정렬 (등록일순)
     if postIds:
         return case(
