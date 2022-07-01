59438e4fb1e4d440ea6a90d464849e24afb43ba5
wally_yoon <bluegmlduf2@naver.com>
2022-07-02 00:17:06 +0900

    0702 미로그인시 게시글의 일정시작일과 일정종료일 취득

--- src/store/post/index.js
+++ src/store/post/index.js
@@ -9,18 +9,37 @@ export default {
     setPost(state, payload) {
       state.post = payload;
     },
+    // 게시글 이전 내용 지우기
+    clearPost(state) {
+      state.post = null;
+    },
   },
   actions: {
     // 게시물 초기화
     setInitPost({ commit }, payload) {
       commit('setSpinner', true); // 스피너 동작
+      commit('clearPost'); // 게시글의 이전 내용 지우기
 
       // 게시물 정보 취득
       return new PostProxy()
         .getPost(payload)
         .then((response) => {
+          // 서버로부터 받은 값
+          const responseData = response.data;
+          // 유저 로그인 정보
+          const isLogined = this.getters.user;
+
+          // 미로그인 상태일시 유저의 로컬스토리지에서 해당 일정의 시작일 종료일이 있는지 검색
+          if (!isLogined) {
+            // 로컬스토리지의 나의 할일일정과 완료일정을 취득후, 해당 일정의 예정 시작일 예정 종료일을 취득
+            const { myTodolist, myCompletelist } = this.getters.selection;
+            const myList = [...myTodolist, ...myCompletelist].find((e) => e.postId === payload);
+            responseData.myStartDate = myList?.myStartDate;
+            responseData.myEndDate = myList?.myEndDate;
+          }
+
           // 서버에서 가져온 게시글을 초기화
-          commit('setPost', response.data);
+          commit('setPost', responseData);
         })
         .catch(() => {
           console.log('Request failed...');
