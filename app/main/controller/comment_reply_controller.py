2653c6fe511e33fa9ffd249da0c9b6121c494dce
wally_yoon <bluegmlduf2@naver.com>
2022-07-12 02:16:30 +0900

    0712 대댓글수정삭제기능추가(서버)

--- app/main/controller/comment_reply_controller.py
+++ app/main/controller/comment_reply_controller.py
@@ -3,15 +3,29 @@ from flask import request
 
 from app.main.util.decorator import token_required
 from ..util.dto import CommentReplyDto
-from ..service.comment_reply_service import create_comment_reply
+from ..service.comment_reply_service import create_comment_reply,update_comment_reply,destroy_comment_reply
 
 api = CommentReplyDto.api
 
-@api.route('')
+@api.route('/<param>')
+@api.param('param', '댓글번호/대댓글번호')
 class CommentReply(Resource):
     @token_required
     @api.doc('대댓글 등록하기')
-    def post(uid,self):
+    def post(uid,self,param):
         """대댓글 정보를 등록"""
         payload = request.json
         return create_comment_reply(uid,payload)
+
+    @token_required
+    @api.doc('대댓글 수정하기')
+    def put(uid,self,param):
+        """대댓글 정보를 수정"""
+        payload = request.json
+        return update_comment_reply(uid,payload)
+
+    @token_required
+    @api.doc('대댓글 삭제하기')
+    def delete(uid,self,param):
+        """대댓글 정보를 삭제"""
+        return destroy_comment_reply(uid,param)
--- app/main/service/comment_reply_service.py
+++ app/main/service/comment_reply_service.py
@@ -37,3 +37,60 @@ def create_comment_reply(uid,param):
         }
         return response_object, 401
 
+def update_comment_reply(uid,param):
+    '''대댓글 수정'''
+    try:
+        user=User.query.filter_by(uid=uid).first()
+        # 기존 유저가 존재할 경우 유저선택정보를 갱신
+        if user:
+            comment_reply = CommentReply.query.filter_by(commentReplyUid=uid, commentReplyId=param['commentReplyId']).first()
+            comment_reply.commentReplyContent = param['commentReplyContent']
+
+            db.session.add(comment_reply)
+            db.session.commit()
+                        
+            response_object = {
+                'status': 'success',
+                'message': '대댓글을 수정했습니다'
+            }
+            return response_object, 201        
+    except exc.IntegrityError as e:
+        response_object = {
+            'status': 'fail',
+            'message': '이미 수정된 대댓글입니다'
+        }
+        return response_object, 401
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': '대댓글 수정중 에러가 발생하였습니다'
+        }
+        return response_object, 401
+
+def destroy_comment_reply(uid,commentReplyId):
+    '''대댓글 삭제'''
+    try:
+        user=User.query.filter_by(uid=uid).first()
+        # 기존 유저가 존재할 경우 유저선택정보를 갱신
+        if user:
+            CommentReply.query.filter_by(commentReplyUid=uid, commentReplyId=commentReplyId).delete()
+
+            db.session.commit()
+                        
+            response_object = {
+                'status': 'success',
+                'message': '대댓글을 삭제했습니다'
+            }
+            return response_object, 201
+    except exc.IntegrityError as e:
+        response_object = {
+            'status': 'fail',
+            'message': '이미 삭제된 대댓글입니다'
+        }
+        return response_object, 401
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': '대댓글 삭제중 에러가 발생하였습니다'
+        }
+        return response_object, 401
