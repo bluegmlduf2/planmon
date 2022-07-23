a6f912b83872b1cba5b397226dec2233cd3ee37e
wally_yoon <bluegmlduf2@naver.com>
2022-07-23 17:08:06 +0900

    0723 불필요한 부분제거

--- app/main/util/decorator.py
+++ app/main/util/decorator.py
@@ -46,25 +46,3 @@ def get_user_by_token(f) -> Callable:
         return f(uid ,*args, **kwargs)
 
     return decorated
-
-def admin_token_required(f: Callable) -> Callable:
-    @wraps(f)
-    def decorated(*args, **kwargs):
-
-        data, status = Auth.get_logged_in_user(request)
-        token = data.get('data')
-
-        if not token:
-            return data, status
-
-        admin = token.get('admin')
-        if not admin:
-            response_object = {
-                'status': 'fail',
-                'message': 'admin token required'
-            }
-            return response_object, 401
-
-        return f(*args, **kwargs)
-
-    return decorated
