dfedbf9b05e308e4c98df32c9daef1241a3a0f1e
wally_yoon <bluegmlduf2@naver.com>
2022-08-07 16:55:52 +0900

    0807 권한인증에대한 에러핸들링

--- app/main/service/auth_helper.py
+++ app/main/service/auth_helper.py
@@ -6,40 +6,31 @@ class Auth:
     @staticmethod
     def check_verified_user(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
         '''인증 유저 확인 '''
-        try:
-            auth_token= data.headers.get('authorization')
-            if auth_token:
-                try:
-                    # 파이어베이스에서 유저정보인증 확인
-                    user = auth.verify_id_token(auth_token.replace("Bearer ",""))
-                    response_object = {
-                        'status': 'success',
-                        'message': '성공적으로 인증되었습니다',
-                        'Authorization': True,
-                        'uid': user.get('uid'),
-                    }
-                    return response_object, 200
-                except:
-                    #유효하지않은 토큰
-                    response_object = {
-                        'status': 'fail',
-                        'message': '유효하지않은 토큰입니다'
-                    }
-                    return response_object, 401
-            else:
+        auth_token= data.headers.get('authorization')
+        if auth_token:
+            try:
+                # 파이어베이스에서 유저정보인증 확인
+                user = auth.verify_id_token(auth_token.replace("Bearer ",""))
+                response_object = {
+                    'status': 'success',
+                    'message': '성공적으로 인증되었습니다',
+                    'Authorization': True,
+                    'uid': user.get('uid'),
+                }
+                return response_object
+            except:
+                #유효하지않은 토큰
                 response_object = {
                     'status': 'fail',
-                    'message': '인증정보가 존재하지않습니다'
+                    'message': '유효하지않은 토큰입니다'
                 }
-                return response_object, 401
-
-        except Exception as e:
-            print(e)
+                return response_object
+        else:
             response_object = {
                 'status': 'fail',
-                'message': '인증진행중 시스템에러 발생'
+                'message': '인증정보가 존재하지않습니다'
             }
-            return response_object, 500
+            return response_object
 
 
 
--- app/main/util/decorator.py
+++ app/main/util/decorator.py
@@ -9,22 +9,27 @@ def token_required(f) -> Callable:
     '''토큰에 유저정보가 존재 필수, uid반환'''
     @wraps(f)
     def decorated(*args, **kwargs):
+        try:
+            data = Auth.check_verified_user(request)
+            token = data.get('Authorization') # 인증여부
+            uid =data.get('uid') # uid
 
-        data, status = Auth.check_verified_user(request)
-        token = data.get('Authorization') # 인증여부
-        uid =data.get('uid') # uid
+            # 인증정보가 존재하지 않을 시 
+            if not token:
+                return getMessage(771),401
+            
+            # 인증되었지만 DB에 유저정보가 없을시 DB에 유저ID 등록
+            if not get_user(uid):
+                save_user(uid)
 
-        # 인증정보가 존재하지 않을 시 
-        if not token:
+            # 인증이 문제없으면 진행
+            return f(uid ,*args, **kwargs)
+        except Exception as e:
+            # 로그 인스턴스 생성
+            logger,uuid,user_ip = ceate_logger()
+            # 에러로그남기기
+            logger.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
             return getMessage(770),401
-        
-        # 인증되었지만 DB에 유저정보가 없을시 DB에 유저ID 등록
-        if not get_user(uid):
-            save_user(uid)
-
-        # 인증이 문제없으면 진행
-        return f(uid ,*args, **kwargs)
-
     return decorated
 
 
@@ -32,18 +37,23 @@ def get_user_by_token(f) -> Callable:
     '''토큰에 유저정보가 존재 필수가아님, uid반환'''
     @wraps(f)
     def decorated(*args, **kwargs):
+        try:
+            data = Auth.check_verified_user(request)
+            uid =data.get('uid') # uid
+            
+            # 인증되었지만 DB에 유저정보가 없을시 DB에 유저ID 등록
+            if uid:
+                if not get_user(uid):
+                    save_user(uid)
 
-        data = Auth.check_verified_user(request)[0]
-        uid =data.get('uid') # uid
-        
-        # 인증되었지만 DB에 유저정보가 없을시 DB에 유저ID 등록
-        if uid:
-            if not get_user(uid):
-                save_user(uid)
-
-        # 인증이 문제없으면 진행
-        return f(uid ,*args, **kwargs)
-
+            # 인증이 문제없으면 진행
+            return f(uid ,*args, **kwargs)
+        except Exception as e:
+            # 로그 인스턴스 생성
+            logger,uuid,user_ip = ceate_logger()
+            # 에러로그남기기
+            logger.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
+            return getMessage(770),401
     return decorated
 
 
@@ -52,9 +62,15 @@ def ceate_logger():
     # logger의 인스턴스 생성
     logger = logging.getLogger('myplanmonlog')
     
+    # 로그고유번호용 UUID 8글자
+    uuid=get_uuid()[:8]
+
+    # 사용자 IP
+    user_ip=request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
+
     # 이미 logger인스턴스에 handlers가 등록된 경우 중복등록을 하지않음(로그중복호출방지)
     if len(logger.handlers) > 0:
-        return logger
+        return logger,uuid,user_ip
 
     # 로그파일 출력설정 및 핸들러등록
     logger.setLevel(logging.INFO) # 로그설정 (INFO까지표시)
@@ -63,7 +79,8 @@ def ceate_logger():
     logHandler.suffix = "%Y%m%d"
     logger.addHandler(logHandler)
 
-    return logger
+    # 로그생성후 로그객체와 로그입력에 필요한 UUID와 IP를 반환한다
+    return logger,uuid,user_ip
 
 
 def exception_handler(f) -> Callable:
@@ -72,13 +89,7 @@ def exception_handler(f) -> Callable:
     def decorated(*args, **kwargs):
         try:
             # 로그 인스턴스 생성
-            logger = ceate_logger()
-
-            # 로그고유번호용 UUID 8글자
-            uuid=get_uuid()[:8]
-
-            # 사용자 IP
-            user_ip=request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
+            logger,uuid,user_ip = ceate_logger()
 
             # 로그 기록
             logger.info(f"[{user_ip}] [{uuid}] 요청 => ({f.__qualname__})")
