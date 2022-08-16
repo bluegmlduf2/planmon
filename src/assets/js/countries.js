6abda5a7d5f184c1843c72664c58f592a89c8e06
wally_yoon <bluegmlduf2@naver.com>
2022-08-16 12:59:56 +0900

    0816 국가미지원메세지수정

--- src/assets/js/countries.js
+++ src/assets/js/countries.js
@@ -4,6 +4,6 @@
  */
 export default [
   { value: 'JP', text: '일본' },
-  { value: 'US', text: '미국' },
-  { value: 'CN', text: '중국' },
+  { value: 'US', text: '미국 (현재 지원하지 않습니다)' },
+  { value: 'CN', text: '중국 (현재 지원하지 않습니다)' },
 ];
