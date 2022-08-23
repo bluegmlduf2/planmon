6035557b0bf4a513fe522dcdea4bfb7b33170026
wally_yoon <bluegmlduf2@naver.com>
2022-08-23 22:26:31 +0900

    0823 일정작성 라우팅부분 함수로 분리/ 일정작성화면에서 새로고침 경고메세지나오지않게수정

--- src/plugins/vue-router.js
+++ src/plugins/vue-router.js
@@ -31,31 +31,40 @@ export const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   if (to.matched.some((m) => m.meta.auth) && !store.getters.user) {
     // 권한이 필요한 화면에서 로그인하지않은 경우 홈으로 이동
-    next({
-      name: 'home.index',
-    });
+    next({ name: 'home.index' });
   } else if (to.name === 'write.index') {
     // 글쓰기 화면에 이동전 필수입력값 확인
-    // || {}은 객체의 destructing의 null에 대비
-    const { country, stayStatus } = to.params.selection || {};
-    // 국가나 체류상태를 선택하지 않은 경우 유저일정설정화면 이동
-    if (!country || !stayStatus) {
-      Vue.prototype.$toast.warning(message.emptyWriteCondition);
-      // 모바일화면 유무
-      if (Vue.prototype.isMobile()) {
-        // 모바일화면인 경우 나의 일정 설정화면으로 이동
-        // Redirected 에러때문에 push사용
-        router.push({ path: 'selection' });
-      }
-    } else {
-      // 글쓰기 화면이동에 문제가 없으면 글쓰기 화면으로이동
-      next();
-    }
+    // eslint-disable-next-line no-use-before-define
+    setWriteRouter(to, from, next);
   } else {
     // 문제가 없는 경우 화면이동
     next();
   }
 });
+
+// 글쓰기 화면 이동관련 라우터
+function setWriteRouter(to, from, next) {
+  // || {}은 객체의 destructing의 null에 대비
+  const { country, stayStatus } = to.params.selection || {};
+  if (!from.name) {
+    // 글쓰기화면에서 새로고침하면 홈화면으로
+    next({ name: 'home.index' });
+  } else if (!country || !stayStatus) {
+    // 국가나 체류상태를 선택하지 않은 경우 유저일정설정화면 이동
+    Vue.prototype.$toast.warning(message.emptyWriteCondition);
+    // 모바일화면 유무
+    if (Vue.prototype.isMobile()) {
+      // 모바일화면인 경우 나의 일정 설정화면으로 이동
+      // Redirected 에러때문에 push사용
+      router.push({ path: 'selection' });
+    } else {
+      next({ name: 'home.index' });
+    }
+  } else {
+    // 글쓰기 화면이동에 문제가 없으면 글쓰기 화면으로이동
+    next();
+  }
+}
 
 Vue.router = router;
 
