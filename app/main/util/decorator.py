0b8170fb044f95a7ff17af92a2f013dfa81a8d43
wally_yoon <bluegmlduf2@naver.com>
2022-08-26 20:21:20 +0900

    0826 에러로그를 새로운 분리해서 신규 생성

--- app/main/util/decorator.py
+++ app/main/util/decorator.py
@@ -28,8 +28,10 @@ def token_required(f) -> Callable:
         except Exception as e:
             # 로그 인스턴스 생성
             logger,uuid,user_ip = ceate_logger()
+            loggerError,_,_ = ceate_logger(True)
             # 에러로그남기기
             logger.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
+            loggerError.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
             return getMessage(770),401
     return decorated
 
@@ -52,16 +54,24 @@ def get_user_by_token(f) -> Callable:
         except Exception as e:
             # 로그 인스턴스 생성
             logger,uuid,user_ip = ceate_logger()
+            loggerError,_,_ = ceate_logger(True)
             # 에러로그남기기
             logger.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
+            loggerError.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
             return getMessage(770),401
     return decorated
 
 
-def ceate_logger():
-    '''로그 인스턴스 생성'''
+def ceate_logger(isErrorLog = False):
+    '''로그 인스턴스 생성한다 파라미터정보(isErrorLog)에 따라 정보로그와 경고로그를 출력'''
+    # 에러로그와 정보로그의 필요한 환경설정파일을 분리
+    config = {
+        'loggerName': 'errorLog' if isErrorLog else  'infoLog',
+        'loggerPath': './log/logErrorfile.log' if isErrorLog else  './log/logfile.log',
+    }
+
     # logger의 인스턴스 생성
-    logger = logging.getLogger('myplanmonlog')
+    logger = logging.getLogger(config['loggerName'])
     
     # 로그고유번호용 UUID 8글자
     uuid=get_uuid()[:8]
@@ -75,7 +85,7 @@ def ceate_logger():
 
     # 로그파일 출력설정 및 핸들러등록
     logger.setLevel(logging.INFO) # 로그설정 (INFO까지표시)
-    logHandler = handlers.TimedRotatingFileHandler('./log/logfile.log', when='midnight', interval=1)
+    logHandler = handlers.TimedRotatingFileHandler(config['loggerPath'], when='midnight', interval=1)
     logHandler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] | %(message)s '))
     logHandler.suffix = "%Y%m%d"
     logger.addHandler(logHandler)
@@ -103,8 +113,11 @@ def exception_handler(f) -> Callable:
             db.session.rollback()
             return e.errorInfo,400
         except Exception as e:
-            # 기타 예외 처리
+            # 로그 인스턴스 생성
+            loggerError,_,_ = ceate_logger(True)
+            # 가터 애로 로그남기기
             logger.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
+            loggerError.exception(f"[{user_ip}] [{uuid}] 에러 => ({e})")
             # 에러발생시 롤백
             db.session.rollback()
             return getMessage(800),500
