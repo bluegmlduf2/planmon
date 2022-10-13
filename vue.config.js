afd4bb2247cb1f84161d54f0b439f31343cf4351
wally_yoon <bluegmlduf2@naver.com>
2022-10-13 23:08:25 +0900

    Revert "0914 프리렌더링적용(동적라우팅부분은생략..)"
    This reverts commit 1d08f0b5d6bb98b5a8e5863a09d6b622718d3a28.

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
-const PrerenderSpaPlugin = require('prerender-spa-plugin');
-
-const productionPlugins = [
-  new PrerenderSpaPlugin({
-    staticDir: process.env.VUE_APP_API_BUILD_PATH,
-    routes: ['/', '/selection', '/alllist', '/todolist', '/completelist'], // 해당 URL을 프리렌더링함
-    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
-      renderAfterElementExists: '#app',
-    }),
-  }),
-];
-
 module.exports = {
   outputDir: process.env.VUE_APP_API_BUILD_PATH, // 배포경로
   lintOnSave: false, // Eslint 끄기
@@ -22,11 +10,5 @@ module.exports = {
         changeOrigin: true,
       },
     },
-  },
-  // 프리렌더링설정을 웹팩에 적용
-  configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(...productionPlugins);
-    }
   },
 };
